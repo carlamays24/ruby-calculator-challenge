893b85dad074c477581f168ca63e07a67fb045a7
providenceri@gmail.com <carlamays24@gmail.com>
2016-04-18 13:35:24 -0400

    made changes

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,32 @@
 require 'pry'
 
-# Calculator Class here
+def Calculator
+  def initialize(start_num)
+    @start_num = start_num.to_f
+  end
+
+  def add(another_num)
+    @start_num += another_num.to_f
+    self
+  end
+
+  def substract(another_num)
+    @start_num -= another_num.to_f
+    self
+  end
+
+  def multiply(another_num)
+    @start_num *= another_num.to_f
+    self
+  end
+
+  def divide(another_num)
+    @start_num /= another_num.to_f
+    self
+  end
+
+end
+
 
 binding.pry
 ''
